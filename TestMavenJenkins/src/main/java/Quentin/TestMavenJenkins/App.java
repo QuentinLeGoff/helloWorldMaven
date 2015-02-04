330d3ac75b0a89a8342982414128ce61ba509d03
Quentin Le Goff <lg.quentin@gmail.Com>
2015-02-04 13:35:30 +0100

    1er push

---
+++ TestMavenJenkins/src/main/java/Quentin/TestMavenJenkins/App.java
@@ -0,0 +1,13 @@
+package Quentin.TestMavenJenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
